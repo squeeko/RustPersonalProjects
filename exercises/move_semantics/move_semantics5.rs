66aa2efe796a63995468a6c24a18e9b2be007dee
Jermaine Allgood <jermaine.allgood@windriver.com>
2022-12-14 14:32:52 -0800

    move and structs

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,14 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
-    *z += 1000;
-    assert_eq!(x, 1200);
+    let mut x = 100; // mutable x 
+    let mut y = &mut x; // y is reference to x and x can only have one reference
+    
+    *y += 100; //derefence y to add 100
+    let z = &mut x; //z = reference to x and it works because y is derefenced
+    *z += 1000; //dereference z to add 1000
+    assert_eq!(x, 1200); // x is 1200
 }
